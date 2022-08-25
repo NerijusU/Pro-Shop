fb226249b33967bf0af0f1f0c0cdf19206b21e2b
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-08-25 15:14:12 +0200

    initial jwt

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+// Validate token
+
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+  let token;
+
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    console.log("token found");
+    console.log(req.headers.authorization);
+  }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+
+  next();
+});
+
+export { protect };
