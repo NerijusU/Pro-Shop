514ea0e28496920746b76e699454a87e01a5af61
Nerijus Urbonas <nerijus.urbonas@gmail.com>
2022-08-27 20:28:18 +0200

    add User registration & password encryption

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -11,16 +11,27 @@ const protect = asyncHandler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
-    console.log("token found");
-    console.log(req.headers.authorization);
+    try {
+      token = req.headers.authorization.split(" ")[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.user = await User.findById(decoded.id).select("-password");
+
+      //   console.log(req.user);
+
+      next();
+    } catch (error) {
+      console.error(error);
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
   }
 
   if (!token) {
     res.status(401);
     throw new Error("Not authorized, no token");
   }
-
-  next();
 });
 
 export { protect };
